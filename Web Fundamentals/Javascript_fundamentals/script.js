a02239cf98b4915a474242fd182ea472a8872cb9
Arbri Berberi <74304861+Arbri-B@users.noreply.github.com>
2023-10-31 12:32:47 +0100

    first commit

---
+++ Web Fundamentals/Javascript_fundamentals/script.js
@@ -0,0 +1,13 @@
+var age = 24;
+var male = true;
+var female = false;
+var name = "Arbri";
+var daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+var person = {
+    'name': 'Arbri'
+    'age' : 24,
+    'email': 'berberiarbri@gmail.com'
+    'languages': ['albanian', 'english', 'italian']
+}
+
