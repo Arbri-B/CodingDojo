a8f28469942e9c725730c34659e7bbee20a4a602
Arbri Berberi <74304861+Arbri-B@users.noreply.github.com>
2023-12-14 12:37:56 +0100

    Added pokemon api

---
+++ MERN/React/PokemonAPI/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Pokemonat from './components/Pokemon'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Pokemonat/>
+    </>
+  )
+}
+
+export default App
---
+++ MERN/React/PokemonAPI/src/components/Pokemon.jsx
@@ -0,0 +1,35 @@
+import React,{useEffect, useState} from "react";
+
+const Pokemonat = () => {
+
+        const [pokemon, setPokemon] = useState([]);
+
+        useEffect(() => {
+            fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+                setPokemon(response.results)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }, [] );
+
+
+    return(
+        <div>
+            <ul>
+                {
+                    pokemon.map((pokemonObj, index) => {
+                        return (<li key={index}>{pokemonObj.name}</li>)
+                    }
+                    )
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Pokemonat;
